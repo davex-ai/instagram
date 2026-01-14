3e95822cf2bf23bb2ffe7707942119d849e042f7
davex-ai <omotoyedave4@gmail.com>
2026-01-15 00:48:21 +0100

    ....

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 const mobileBackgroundColor = Color.fromARGB(0, 0, 0, 1);
-const webBackgroundColor = Color.fromARGB(10, 10, 10, 1);
-const mobileSearchColor = Color.fromARGB(30, 30, 30, 1);
-const blueColor = Color.fromARGB(0,540, 246, 1);
+const webBackgroundColor = Color.fromARGB(18, 18, 18, 1);
+const mobileSearchColor = Color.fromARGB(38, 38, 38, 1);
+const blueColor = Color.fromARGB(0,149, 246, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
