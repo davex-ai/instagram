ed6a2dc73181caf74e5b5fcc7787816d63b4bfec
davex-ai <omotoyedave4@gmail.com>
2026-01-30 15:11:03 +0100

    login.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:instagram/firebase_options.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout_screen.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
+import 'package:instagram/screens/login.dart';
 import 'package:instagram/utils/color.dart';
 
 void main() async {
@@ -27,10 +28,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor
         ),
-        home: const ResponsiveLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout(),
-        ),
+        home: Login(),
+        // home: const ResponsiveLayout(
+        //   webScreenLayout: WebScreenLayout(),
+        //   mobileScreenLayout: MobileScreenLayout(),
+        // ),
     );
   }
 }
