f5ab654ae80674f9e95a648d4280f93a00f86c73
davex-ai <omotoyedave4@gmail.com>
2026-01-31 11:25:48 +0100

    login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/flutter_svg.dart";
+import "package:instagram/utils/color.dart";
 
 class Login extends StatefulWidget {
   const Login ({Key? key}) : super(key: key);
@@ -11,7 +13,18 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Login "),
+      body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset("assets/ic_instagram.svg", color : primaryColor, height: 64,),
+                const SizedBox(height: 64,)
+              ],
+            ),
+      ))
     );
   }
 }
