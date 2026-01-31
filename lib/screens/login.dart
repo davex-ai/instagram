4ed8849ed25f5b9c21474a57aa77ba26f764c45c
davex-ai <omotoyedave4@gmail.com>
2026-01-31 17:41:34 +0100

    login ui

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
 import "package:instagram/utils/color.dart";
+import "package:instagram/widget/text_input.dart";
 
 class Login extends StatefulWidget {
   const Login ({Key? key}) : super(key: key);
@@ -10,6 +11,15 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +30,43 @@ class _LoginState extends State<Login> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                Flexible(child: Container(), flex: 2,),
                 SvgPicture.asset("assets/ic_instagram.svg", color : primaryColor, height: 64,),
-                const SizedBox(height: 64,)
+                const SizedBox(height: 64,),
+                TextInput(textEditingController: _emailController, hintText: "Enter your email", textInputType: TextInputType.emailAddress),
+                const SizedBox(height: 34,),
+                TextInput(textEditingController: _passwordController, hintText: "Enter your password", textInputType: TextInputType.text, isPass: true),
+                const SizedBox(height: 34,),
+                InkWell(
+                  child: Container(
+                  child: const Text("Log In"),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.all(
+                      Radius.circular(4),
+                    )
+                  ),
+                   color: blueColor
+                  ),
+                ),),
+                const SizedBox( height: 12,),
+                Flexible(child: Container(), flex: 2,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                      child: const Text("Sign Up", style: TextStyle(fontWeight: FontWeight.bold),
+                    ), padding: const EdgeInsets.symmetric(vertical: 8),
+                    ))],
+                )
               ],
             ),
       ))
