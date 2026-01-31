afd0d5f71a53df64237c4eef2d6fda3891f2f7f8
davex-ai <omotoyedave4@gmail.com>
2026-02-01 00:45:27 +0100

    login logic

--- lib/logic/authentication.dart
+++ lib/logic/authentication.dart
@@ -1,18 +1,14 @@
-
-
-
 import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:instagram/logic/storage.dart';
 
 class Authentication {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  signUpUser({
+  Future<String> signUpUser({
     required String email,
     required String password,
     required String username,
@@ -38,10 +34,29 @@ class Authentication {
     } on FirebaseAuthException catch (err){
       if (err.code == "invalid-email"){
         res = "Invalid Email";
-      } else if(err.code == "weak-password")
+      } else if(err.code == "weak-password"){
         res = "Weak Password";
+      }
     }
     catch(err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
+  Future<String> loginUser({
+    required String email,
+    required String password
+}) async {
+    String res = "Some Error Occurred";
+    try {
+      if(email.isNotEmpty || password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(email: email, password: password);
+        res = "Successfully logged In";
+      } else {
+        res = "Please fill all field";
+      }
+    } catch (err) {
       res = err.toString();
     }
     return res;
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
+import "package:instagram/logic/authentication.dart";
 import "package:instagram/utils/color.dart";
+import "package:instagram/utils/utils.dart";
 import "package:instagram/widget/text_input.dart";
 
 class Login extends StatefulWidget {
@@ -13,13 +15,31 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _loaded = false;
 
   @override
   void dispose() {
+
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
   }
+
+  void loginUser() async {
+    setState(() {
+      _loaded = true;
+    });
+    String response = await Authentication().loginUser(email: _emailController.text, password: _passwordController.text);
+    if(response == "Successfully logged In"){
+
+    } else {
+      showSnackBar(response, context);
+    }
+    setState(() {
+      _loaded = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,9 +58,11 @@ class _LoginState extends State<Login> {
                 TextInput(textEditingController: _passwordController, hintText: "Enter your password", textInputType: TextInputType.text, isPass: true),
                 const SizedBox(height: 24,),
                 InkWell(
-                  onTap: () {},
+                  onTap: loginUser,
                   child: Container(
-                  child: const Text("Log In"),
+                  child: _loaded ? const Center(
+                    child: CircularProgressIndicator( color: primaryColor,),
+                  ) : const Text("Log In"),
                   width: double.infinity,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
