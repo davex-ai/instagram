719ab79a6530108fed697fab61521896a35faf3b
davex-ai <omotoyedave4@gmail.com>
2026-02-01 23:22:25 +0100

    feat:comment

--- lib/screens/comment.dart
+++ lib/screens/comment.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/logic/firestoreMethods.dart';
 import 'package:instagram/models/user.dart';
@@ -15,6 +16,14 @@ class Comment extends StatefulWidget {
 }
 
 class _CommentState extends State<Comment> {
+  final TextEditingController _commentControl = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _commentControl.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
@@ -24,7 +33,26 @@ class _CommentState extends State<Comment> {
         title: const Text('Comment'),
         centerTitle: false,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.snap['postId'])
+            .collection('comments')
+            .orderBy('datePublished', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) => CommentCard(
+              snap: (snapshot.data! as dynamic).doc[index].data(),
+            ),
+          );
+        },
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
@@ -42,6 +70,7 @@ class _CommentState extends State<Comment> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 16, right: 8),
                   child: TextField(
+                    controller: _commentControl,
                     decoration: InputDecoration(
                       hintText: 'Comment as ${user.username}',
                       border: InputBorder.none,
@@ -53,11 +82,14 @@ class _CommentState extends State<Comment> {
                 onTap: () async {
                   await Firestoremethods().postComment(
                     widget.snap['postId'],
-                    widget.snap['text'],
+                    _commentControl.text,
                     user.uid,
                     user.username,
                     user.photoURL,
                   );
+                  setState(() {
+                    _commentControl.text = '';
+                  });
                 },
                 child: Container(
                   padding: const EdgeInsets.symmetric(
--- lib/widget/comment_card.dart
+++ lib/widget/comment_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CommentCard extends StatefulWidget {
-  const CommentCard({super.key});
+  final snap;
+  const CommentCard({super.key, required this.snap});
 
   @override
   State<CommentCard> createState() => _CommentCardState();
@@ -14,35 +16,44 @@ class _CommentCardState extends State<CommentCard> {
       padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
       child: Row(
         children: [
-          CircleAvatar(backgroundImage: NetworkImage('url'), radius: 18),
+          CircleAvatar(
+            backgroundImage: NetworkImage(widget.snap['profilePic']),
+            radius: 18,
+          ),
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'username',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: widget.snap['name'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(text: '${widget.snap['text']}'),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      DateFormat.yMMMd().format(
+                        widget.snap['datePublished'].toDate(),
                       ),
-                      TextSpan(text: 'desc'),
-                    ],
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    'date',
-                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )),
+          ),
           Container(
             padding: const EdgeInsets.all(8),
             child: const Icon(Icons.favorite, size: 16),
